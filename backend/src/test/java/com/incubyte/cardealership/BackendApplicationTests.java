dcd111cea11df0c7ecc8b513657d8c417a88bd8e
DShrujal17 <shrujaldoshi907@gmail.com>
2026-07-10 23:10:42 +0530

    chore: initialize Spring Boot backend project

---
+++ backend/src/test/java/com/incubyte/cardealership/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incubyte.cardealership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
